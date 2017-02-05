5a45621abc53059257ccf6da75938c53560d7274
Hobo Noir <hobonetics@gmail.com>
2017-02-05 16:44:08 -0500

    stubbed methods and algorithm

--- src/seamcarve/MyPicturePane.java
+++ src/seamcarve/MyPicturePane.java
@@ -1,18 +1,21 @@
 package seamcarve;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import support.seamcarve.*;
 
-
-/** 
- * This class is your seam carving picture pane.  It is a subclass of PicturePane,
- * an abstract class that takes care of all the drawing, displaying, carving, and
- * updating of seams and images for you.  Your job is to override the abstract
- * method of PicturePanel that actually finds the lowest cost seam through
- * the image.
+/**
+ * This class is your seam carving picture pane. It is a subclass of
+ * PicturePane, an abstract class that takes care of all the drawing,
+ * displaying, carving, and updating of seams and images for you. Your job is to
+ * override the abstract method of PicturePanel that actually finds the lowest
+ * cost seam through the image.
  * 
- * See method comments and handouts for specifics on the steps of the seam carving algorithm.
+ * See method comments and handouts for specifics on the steps of the seam
+ * carving algorithm.
  *
  * 
  * @version 01/07/2017
@@ -20,12 +23,12 @@ import support.seamcarve.*;
 
 public class MyPicturePane extends PicturePane {
 	private int[][] _pixelArray;
+	private int[][] _colorImportance;
 	private String _picture;
-	private int _colorValue;
-	
+
 	/**
-	 * The constructor accepts an image filename as a String and passes
-	 * it to the superclass for displaying and manipulation.
+	 * The constructor accepts an image filename as a String and passes it to
+	 * the superclass for displaying and manipulation.
 	 * 
 	 * @param pane
 	 * @param filename
@@ -35,86 +38,108 @@ public class MyPicturePane extends PicturePane {
 		String defaultFileName = "/Users/Akhil/Pictures/honey.jpg";
 		defaultFileName = filename;
 		filename = _picture;
-		_pixelArray = new int[getPicWidth()][getPicWidth()];
-		calculateColorImportance();
+		_pixelArray = new int[getPicWidth()][getPicHeight()];
+		_colorImportance = new int[getPicWidth()][getPicHeight()];
 	}
 
-	
 	/**
-	 * In this method, you'll implement the dynamic programming algorithm
-	 * that you learned on the first day of class to find the lowest cost seam from the top
-	 * of the image to the bottom. BEFORE YOU START make sure you fully understand how the algorithm works
-	 * and what it's doing.
-	 * See the handout for some helpful resources and use hours/piazza to clarify conceptual blocks
-	 * before you attempt to write code.
+	 * In this method, you'll implement the dynamic programming algorithm that
+	 * you learned on the first day of class to find the lowest cost seam from
+	 * the top of the image to the bottom. BEFORE YOU START make sure you fully
+	 * understand how the algorithm works and what it's doing. See the handout
+	 * for some helpful resources and use hours/piazza to clarify conceptual
+	 * blocks before you attempt to write code.
 	 * 
-	 * This method returns an array of ints that represents a seam.  This size of this array
-	 * is the height of the image.  Each entry of the seam array corresponds to one row of the 
-	 * image.  The data in each entry should be the x coordinate of the seam in this row.  
-	 * For example, given the below "image" where s is a seam pixel and - is a non-seam pixel
+	 * This method returns an array of ints that represents a seam. This size of
+	 * this array is the height of the image. Each entry of the seam array
+	 * corresponds to one row of the image. The data in each entry should be the
+	 * x coordinate of the seam in this row. For example, given the below
+	 * "image" where s is a seam pixel and - is a non-seam pixel
 	 * 
-	 * - s - -  
-	 * s - - -  
-	 * - s - -  
-	 * - - s -  
+	 * - s - - s - - - - s - - - - s -
 	 * 
 	 * 
 	 * the following code will properly return a seam:
 	 * 
-	 * int[] currSeam = new int[4];
-	 * currSeam[0] = 1;
-	 * currSeam[1] = 0;
-	 * currSeam[2] = 1;
-	 * currSeam[3] = 2;
-	 * return currSeam;
+	 * int[] currSeam = new int[4]; currSeam[0] = 1; currSeam[1] = 0;
+	 * currSeam[2] = 1; currSeam[3] = 2; return currSeam;
 	 * 
 	 *
 	 * @return the lowest cost seam of the current image
- 	 */
+	 */
 	protected int[] findLowestCostSeam() {
 		// TODO: Your code here
 		return null;
 	}
-	
-	public void calculateColorImportance(){
-		for (int i = 0; i < getPicWidth()-1; i++){
-			for (int j = 0; j < getPicHeight()-1;j++){
-			int	currPixel = _pixelArray[i][j];
-			// use helper method to calculate the color value 
-			Color currColor = getPixelColor(i,j);
-			System.out.println(currColor);
-			
-			//int currColorValue = (int) (currColor.getRed() + currColor.getGreen() + currColor.getBlue());
-			int currColorValue = 0;
-			// does this method actually compute the color value?
-			currColorValue = (int) (currColor.getRed() + currColor.getGreen() + currColor.getBlue());
-			int right = _pixelArray[i+1][j];
-			int left = _pixelArray[i-1][j];
-			int up = _pixelArray[i][j-1];
-			int down = _pixelArray[i][j+1];
-			
-			//getColorValue(currColorValue, currColor);
-			
+
+	public void calculateColorImportance() {
+		for (int i = 0; i < getPicWidth(); i++) {
+			for (int j = 0; j < getPicHeight() - 1; j++) {
+				int currPixel = _pixelArray[i][j];
+				// use helper method to calculate the color value
+				Color currColor = getPixelColor(i, j);
+				int colorValue = (int) (currColor.getRed() + currColor.getBlue() + currColor.getGreen());
+
+				// get adjacent pixels:
+				ArrayList neighbors = getAdjacentPixels(i, j);
+
+				// iterate on neighbors to calculate color importance
+				/*
+				 * _colorImportance[i][j] = something
+				 */
+				for (Iterator<Integer> n = neighbors.iterator(); n.hasNext();) {
+					Integer neighbor = n.next();
+					// TODO -- write code here
+					// calculate difference magnitude between pixel and neighbor
+					// here
+
+					// get color of neighbor (see line 80,81)
+					int neighbColor = -1;
+
+					// absolute value of difference between two
+					int diff = Math.abs(colorValue - neighbColor);
+
+					// put in values array
+				}
 			}
 		}
 	}
-	
-	public int getColorValue(int sum, Color color){
-		// why is the sum of the colors a double?
-			 sum =  (int) (color.getRed() + color.getGreen() + color.getBlue());
-				return sum;
-			}
-		
-	public int getColorSum(int i, int j){
-			//int x = _pixelArray[i][j];
-			Color color = getPixelColor(i,j);
-			_colorValue = (int) (color.getRed() + color.getBlue() + color.getGreen());
- 			return _colorValue;
+
+	public ArrayList getAdjacentPixels(int row, int col) {
+		/*
+		 * return a list of the pixels adjacent to pixel(row, col)
+		 * 
+		 * can vary between 2-4
+		 */
+		// TODO -- do code here
+		ArrayList<Integer> neighbs = new ArrayList();
+		// top
+		try {
+			neighbs.add(new Integer(_pixelArray[row][col - 1]));
+		} catch (IndexOutOfBoundsException whatever) {
+
+		}
+		// right
+
+		// bottom
+
+		// left
+
+		return neighbs;
 	}
+
+	public void calcCostsAndDirs() {
+		// TODO -- write code here
+		// costs[row][col] = min(costs[row+1][col-1 to col+1]) + vals[row]..ish
+
+		// fill in costs array && dirs array
 		
-	public void findColorDifference(int i, int j){
+		//costs is same size as vals array
 		
-				int x = _pixelArray[i][j];		
+		//dirs has same height as width of costs array
+		//dirs has width = height of costs array - 1
+		//single row in dirs = directions for a single seam from picture
+	
 	}
 
 }
